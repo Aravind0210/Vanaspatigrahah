26a98cefc37e1833feff05f82797a13d63b39354
Aravind0210 <aravindmurugesan260@gmail.com>
2025-04-03 15:46:24 +0530

    rework3

--- Vanaspatigrahah_UI/src/app/homepage/homepage.component.ts
+++ Vanaspatigrahah_UI/src/app/homepage/homepage.component.ts
@@ -16,9 +16,6 @@ export class HomepageComponent implements OnInit {
   searchQuery = "";
   showSearchResults = false;
   shops: any[] = [];
-  paginatedShops: any[] = [];
-  currentPage = 1;
-  itemsPerPage = 6;
   backgrounds = [
     'https://images.unsplash.com/photo-1524247108137-732e0f642303?w=600&auto=format&fit=crop&q=60',
     'https://plus.unsplash.com/premium_photo-1679765926730-78765a8b7802?w=600&auto=format&fit=crop&q=60'
@@ -43,8 +40,6 @@ export class HomepageComponent implements OnInit {
         next: (data) => {
           this.shops = data;
           this.showSearchResults = this.shops.length > 0;
-          this.currentPage = 1;
-          this.updatePagination();
           console.log("Shops Loaded:", this.shops);
         },
         error: (error) => {
@@ -54,34 +49,6 @@ export class HomepageComponent implements OnInit {
     } else {
       this.showSearchResults = false;
     }
-  }
-
-  updatePagination() {
-    const startIndex = (this.currentPage - 1) * this.itemsPerPage;
-    this.paginatedShops = this.shops.slice(startIndex, startIndex + this.itemsPerPage);
-    console.log("Paginated Shops:", this.paginatedShops);
-  }
-
-  nextPage() {
-    if (this.currentPage < this.totalPages) {
-      this.currentPage++;
-      this.updatePagination();
-      console.log("Next Page:", this.currentPage);
-    }
-  }
-
-  prevPage() {
-    if (this.currentPage > 1) {
-      this.currentPage--;
-      this.updatePagination();
-      console.log("Previous Page:", this.currentPage);
-    }
-  }
-
-  get totalPages() {
-    const pages = Math.ceil(this.shops.length / this.itemsPerPage);
-    console.log("Total Pages:", pages);
-    return pages;
   }
 
   onHeaderSearch(query: string) {
